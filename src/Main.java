abe309d3bbd5912f55911e03828ff5761dc9c0d0
VeronikaBurkovskaya <37294035+VeronikaBurkovskaya@users.noreply.github.com>
2022-12-17 12:49:41 +0200

    Lesson 29

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-
-        //System.out.println("Hello Veronika!");
 
         /*int my_number = 5;
         double second_number;
@@ -57,7 +56,7 @@ public class Main {
         System.out.println(a==b);
         System.out.println(a!=b);*/
 
-        int age = 17;
+/*int age = 17;
 
         if (age>=18) {
             System.out.println("You are an old person!");
@@ -96,6 +95,69 @@ public class Main {
             default:
                 System.out.println("No such day");
         }
+*/
+
+        /*int k = 0;
+
+        while (k<5)
+        {
+            System.out.println(k);
+            k++;
+        }*/
+
+
+
+        // s = console.nextLine();
+
+        //System.out.println("My string " + s );
+
+        /*while (true)
+        {
+            String s_new = console.nextLine();
+            if (s_new.equals("exit"))
+            {
+                break;
+            }
+        }*/
+
+        /*int i = 5;
+        do {
+            System.out.println(i);
+            i--;
+        }while (i>0);
+
+        System.out.println("---------");
+
+        int l = 5;
+        while (l>0)
+        {
+            System.out.println(l);
+            l--;
+        }*/
+
+        int result = 0;
+        for (int i = 1; i<=5; i++){
+
+            if ((i%2) == 0){
+                System.out.println(i);
+                continue;
+            }
+            result = result + i;
+        }
+
+        Scanner console = new Scanner(System.in);
+        String s = console.nextLine();
+        int s_1 = Integer.parseInt(s);
+
+        System.out.println(s+1);
+        System.out.println(s_1+1);
+
+
+
+
+
+
+
 
 
     }
